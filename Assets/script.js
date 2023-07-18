e70e88435412cba8a1794a676d65ceb6353accf8
Arsalan <arsalank.ansari@gmail.com>
2023-07-18 16:37:26 -0400

    prompt aded

--- Assets/script.js
+++ Assets/script.js
@@ -1,13 +1,18 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function genertePassword() {
+  var passwordLength = prompt(
+    "How many characters do you want the password to be?"
+  );
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
